4a0143bcb4de6b58930dba50343249bba554ef67
Mircea Lungu <mircea.lungu@gmail.com>
2021-11-12 14:05:36 +0100

    ....

--- src/Exercises.js
+++ src/Exercises.js
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import Parse from "parse";
+
 export function Exercises() {
-  return <>Let's practice some Danish!</>;
+  const [word, setWord] = useState();
+
+  useEffect(() => {
+    const Word = Parse.Object.extend("Word");
+    const query = new Parse.Query(Word);
+
+    const words = query.find().then((words) => {
+      console.log(words);
+      const randomWord = words[Math.floor(Math.random() * words.length)];
+      console.log(randomWord.get("word"));
+      console.log(randomWord.get("translation"));
+      setWord(randomWord);
+    });
+  }, []);
+
+  return (
+    <>
+      Let's practice some Danish!
+      <h1>
+        Do youp know the meaning of <b>{word.get("word")}</b>?{" "}
+      </h1>
+    </>
+  );
 }
