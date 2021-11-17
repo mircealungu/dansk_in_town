39fa89ee51779da1c2aa1072c108171151d2c061
Mircea Lungu <mircea.lungu@gmail.com>
2021-11-17 11:23:24 +0100

    uploading images too

--- src/Upload.js
+++ src/Upload.js
@@ -6,15 +6,25 @@ import Parse from "parse";
 export function Upload() {
   const [from, setFrom] = useState();
   const [to, setTo] = useState();
+  const [imageFile, setImageFIle] = useState();
 
   async function handleUpload(e) {
     e.preventDefault();
+
+    const Image = Parse.Object.extend("Image");
+    const newImage = new Image();
+
+    const file = new Parse.File(imageFile.name, imageFile);
+    newImage.set("file", file);
 
     const Translation = Parse.Object.extend("Translation");
     const newTranslation = new Translation();
     newTranslation.set("from", from);
     newTranslation.set("to", to);
     newTranslation.set("user", Parse.User.current());
+
+    newTranslation.set("image", newImage);
+
     try {
       await newTranslation.save();
     } catch (error) {
@@ -22,9 +32,23 @@ export function Upload() {
     }
   }
 
+  function handleFileUpload(e) {
+    console.log(e.target);
+    console.dir(e.target);
+    console.log(e.target.files[0]);
+    console.log(e.target.result);
+
+    setImageFIle(e.target.files[0]);
+  }
+
   return (
     <>
       <Form>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Select an image from your computer</Form.Label>
+          <Form.Control onChange={handleFileUpload} type="file" />
+        </Form.Group>
+
         <Form.Group className="mb-3" controlId="formBasicUsername">
           <Form.Label>Word</Form.Label>
           <Form.Control type="text" onChange={(e) => setFrom(e.target.value)} />
