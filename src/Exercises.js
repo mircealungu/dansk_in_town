a974533bdbc15f4c6f1857fa793633adb4725923
Mircea Lungu <mircea.lungu@gmail.com>
2021-11-26 00:35:17 +0100

    Update Exercises.js

--- src/Exercises.js
+++ src/Exercises.js
@@ -2,8 +2,11 @@ import { useEffect, useState } from "react";
 
 import { getTranslations } from "./db/db";
 
+import { Button } from "react-bootstrap";
+
 export function Exercises() {
   const [translation, setTranslation] = useState();
+  const [showingSolution, setShowingSolution] = useState(false);
 
   useEffect(() => {
     getTranslations().then((translations) => {
@@ -22,11 +25,32 @@ export function Exercises() {
       <h1>
         <img
           alt=""
-          style={{ maxWidth: "100%" }}
+          style={{ maxWidth: "100%", maxHeight: "320px" }}
           src={translation.get("image").get("file").url()}
         />
-        <b>{translation.get("from")}</b> = ?{" "}
+        <br />
+        <b>{translation.get("from")}</b> ={" "}
+        {showingSolution ? translation.get("to") : "?"}
       </h1>
+
+      {showingSolution ? (
+        <>
+          <Button onClick={() => window.location.reload(false)}>Next</Button>
+        </>
+      ) : (
+        <div style={{ align: "center" }}>
+          <Button
+            onClick={() => window.location.reload(false)}
+            variant="secondary"
+          >
+            Too Easy
+          </Button>
+          &nbsp;
+          <Button onClick={() => setShowingSolution(true)}>
+            Show Solution
+          </Button>
+        </div>
+      )}
     </>
   );
 }
