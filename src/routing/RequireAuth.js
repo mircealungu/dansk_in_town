612967c2d8628c251c2976ada5619ffe7f39f98d
Mircea Lungu <mircea.lungu@gmail.com>
2021-11-28 01:08:07 +0100

    Update RequireAuth.js

--- src/routing/RequireAuth.js
+++ src/routing/RequireAuth.js
@@ -1,6 +1,7 @@
 import { User } from "parse";
-
 import { Navigate, Outlet } from "react-router";
+
+// based on: https://stackoverflow.com/a/69592617/1200070
 
 export default function RequireAuth() {
   if (!User.current()) {
