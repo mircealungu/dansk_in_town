c9fc2806e93e296ccc40154e3f7387315b408b5c
Mircea Lungu <mircea.lungu@gmail.com>
2021-11-29 19:33:04 +0100

    Update main.js

--- src/cloud/main.js
+++ src/cloud/main.js
@@ -1,9 +1,9 @@
-import Parse from "parse";
+const fetch = require("node-fetch");
 
 Parse.Cloud.define("google_translate", async (request) => {
-  let url = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`;
+  let url = `https://translation.googleapis.com/language/translate/v2?key=${process.env.API_KEY}`;
   url += `&format=text&source=da&target=en&q=${encodeURIComponent(
-    translation.from
+    request.params.word_to_translate
   )}`;
 
   response = await fetch(url);
